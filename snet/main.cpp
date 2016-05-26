e2b285e73b85565ebdfb1e896e95f1b6700114fe
San <san@SanHotMac.local>
2016-05-26 10:34:22 +0800

    add server config

--- snet/main.cpp
+++ snet/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <signal.h>
+#include <vector>
 #include "IOLoop.h"
 #include "http/HttpServer.h"
 #include "http/json.hpp"
@@ -20,10 +21,26 @@
     #define DB_DRIVER "/usr/local/Cellar/freetds/0.95.80/lib/libtdsodbc.so"
 #endif
 #define MAX_CONTENT_LEN 0x100000
-#define NET_CONFIG_FILE "config"
+#define EXEC_CONFIG_FILE "exec_config"
+#define NET_CONFIG_FILE "net_config"
+#define DB_CONFIG_FILE "db_config"
+
+#define FLAG_QUERY 0
+#define FLAG_EXEC 1
+
+nlohmann::json g_si_config;
+nlohmann::json g_exec_config;
+Mutex g_mutex;
 
 struct ServerInfo {
+    ServerInfo() {}
     ServerInfo(const char* svr, const char* db, const char* user, const char* password) {
+        server.assign(svr);
+        database.assign(db);
+        uid.assign(user);
+        pwd.assign(password);
+    }
+    ServerInfo(std::string svr, std::string db, std::string user, std::string password) {
         server.assign(svr);
         database.assign(db);
         uid.assign(user);
@@ -35,32 +52,82 @@ struct ServerInfo {
     std::string pwd;
 };
 
-int db_odbc_exec(const ServerInfo& si, const char *sql, Buffer *content) {
+int readConfig(const char* path, nlohmann::json& config) {
+    char b[1024] = {0};
+    FILE *fp = fopen(path, "rb");
+    if(fp == nullptr) {
+        LOG_STDOUT("%s OpenError!", path);
+        return -1;
+    }
+    fread(b, sizeof(char), 1024, fp);
+    fclose(fp);
+    try {
+        config = nlohmann::json::parse(b);
+    }
+    catch(std::invalid_argument const& e) {
+        LOG_STDOUT("%s Incomplete!", path);
+        return -1;
+    }
+    return 0;
+}
+
+int readUrlConfig(const std::string& url, ServerInfo& si, int& flag, std::string& sql) {
+    nlohmann::json si_config;
+    nlohmann::json exec_config;
+    {
+        MUTEXGUARD_T(g_mutex);
+        si_config = g_si_config;
+        exec_config = g_exec_config;
+    }
+
+    if(exec_config.find(url) == exec_config.end())
+        return -1;
+
+    try {
+        nlohmann::json j_exec = exec_config[url];
+        std::string si_name = j_exec["db"];
+        nlohmann::json j_db = si_config[si_name];
+
+        si = ServerInfo(j_db["host"], j_db["db"], j_db["uid"], j_db["pwd"]);
+        flag = j_exec["exec"] == "query"? FLAG_QUERY:FLAG_EXEC;
+        sql = j_exec["sql"];
+    }
+    catch(std::domain_error const& e) {
+        LOG_STDOUT("readUrlConfig(%s): %s", url.c_str(), e.what());
+        return -1;
+    }
+
+    return 0;
+}
+
+int db_odbc_exec(const ServerInfo &si, int flag, const char *sql, Buffer *content) {
     try {
         char dsn[512] = {0};
         char ct = ';';
         char lf = '\n';
         sprintf(dsn, "Driver={" DB_DRIVER "};Server=%s;Database=%s;UID=%s;PWD=%s;",
-                si.server.c_str(), si.database.c_str(),si.uid.c_str(),si.pwd.c_str());
+                si.server.c_str(), si.database.c_str(), si.uid.c_str(), si.pwd.c_str());
         nanodbc::connection conn(NANODBC_TEXT(dsn));
         long batch_size = 1;
-        nanodbc::result row = execute(conn, NANODBC_TEXT(sql), batch_size);
         LOG_STDOUT("DB_EXEC(%s): Start exec", si.database.c_str());
-        for (int i = 1; row.next(); ++i) {
-            for(int j = 0; j < row.columns(); ++j) {
-                std::string col = "";
-                if(!row.is_null(j)) {
-                    col = row.get<nanodbc::string_type>(j);
-                    content->write((void*)col.c_str(), col.length());
+        nanodbc::result row = execute(conn, NANODBC_TEXT(sql), batch_size);
+        if (flag == FLAG_QUERY) {
+            for (int i = 1; row.next(); ++i) {
+                for (int j = 0; j < row.columns(); ++j) {
+                    std::string col = "";
+                    if (!row.is_null(j)) {
+                        col = row.get<nanodbc::string_type>(j);
+                        content->write((void *) col.c_str(), col.length());
+                    }
+                    if (j != row.columns() - 1)
+                        content->write((void *) &ct, 1);
+                    else
+                        content->write((void *) &lf, 1);
                 }
-                if(j != row.columns()-1)
-                    content->write((void*)&ct, 1);
-                else
-                    content->write((void*)&lf, 1);
             }
+            LOG_STDOUT("DB_EXEC(%s): Recv %d", si.database.c_str(), (int) content->offset());
+            content->buffer()[content->offset()] = '\0';
         }
-        LOG_STDOUT("DB_EXEC(%s): Recv %d", si.database.c_str(), (int)content->offset());
-        content->buffer()[content->offset()] = '\0';
     }
     catch (std::runtime_error const& e) {
         std::string ret(e.what());
@@ -73,6 +140,10 @@ int db_odbc_exec(const ServerInfo& si, const char *sql, Buffer *content) {
 
 void callback(uint8_t method, const std::string &url, HttpResponse *res) {
     Buffer content;
+    std::string sql;
+    ServerInfo si;
+    int flag;
+
     if (url == "/") {
         res->setStatusCode(HttpResponse::HTTP_OK);
         res->setContentType(CONTEXT_TYPE_HTML);
@@ -81,40 +152,10 @@ void callback(uint8_t method, const std::string &url, HttpResponse *res) {
         get_now(date);
         res->setBody("<html><head><title>JOINTCOM</title></head>"
                              "<body><h1>Welcom to JOINTCOM</h1>Now is " + std::string(date) +
-                             "</body></html>");
+                     "</body></html>");
     }
-    else if (url == "/user") {
-        //"10.204.118.102,1433";
-        //"122.224.77.122,1433";
-        ServerInfo si("192.168.0.3,1433", "original", "jc#15User", "No19@Data");
-        int ret = db_odbc_exec(si, "select * from tb_DFL_info", &content);
-        if(ret == -1) {
-            res->setHttp404Status(content.buffer());
-            return;
-        }
-        res->setStatusCode(HttpResponse::HTTP_OK);
-        res->setContentType(CONTEXT_TYPE_PLAIN);
-        res->addHeader("Server", JOINTCOM_FLAG);
-        res->setBody(content.buffer());
-    }
-    else if (url == "/pim") {
-        ServerInfo si("192.168.0.253,1433", "TestPim", "pim", "!Q2w3e4r");
-        int ret = db_odbc_exec(si, "select * from configtable", &content);
-        if(ret == -1) {
-            res->setHttp404Status(content.buffer());
-            return;
-        }
-        res->setStatusCode(HttpResponse::HTTP_OK);
-        res->setContentType("text/plain");
-        res->addHeader("Server", JOINTCOM_FLAG);
-        res->setBody(content.buffer());
-    }
-    else if (url == "/query") {
-        ServerInfo si("192.168.0.220,1433", "JointComV2", "sa", "jointwis");
-        int ret = db_odbc_exec(si, "select KHDM, CPXH, KHXH, AddTime from tdb_bz_khtmdy "
-                               "where KHDM <> ' ' and CPXH in "
-                               "(select CPXH from tdb_cp_tsxx where XHBM = 'jcft252604n002-a1')"
-                               , &content);
+    else if(readUrlConfig(url, si, flag, sql) == 0) {
+        int ret = db_odbc_exec(si, flag, sql.c_str(), &content);
         if(ret == -1) {
             res->setHttp404Status(content.buffer());
             return;
@@ -133,24 +174,17 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, Jointcom!\n";
 
     nlohmann::json config = {{"server", "127.0.0.1"}, {"port", 8080}, {"thread", 1}};
-    if(isFileExist("config") != 0) {
-        std::string s = config.dump();
-        FILE *fp = fopen(NET_CONFIG_FILE, "wb");
-        fwrite(s.c_str(), sizeof(char), s.length(), fp);
-        fclose(fp);
+    if(readConfig(NET_CONFIG_FILE, config)) {
+        LOG_STDOUT( NET_CONFIG_FILE " Error!");
+        return -1;
     }
-    else {
-        char b[128] = {0};
-        FILE *fp = fopen(NET_CONFIG_FILE, "rb");
-        fread(b, sizeof(char), 128, fp);
-        fclose(fp);
-        try {
-            config = nlohmann::json::parse(b);
-        }
-        catch(std::invalid_argument const& e) {
-            std::cout << "Config File Error!\n";
-            return -1;
-        }
+    if(readConfig(DB_CONFIG_FILE, g_si_config)) {
+        LOG_STDOUT( DB_CONFIG_FILE " Error!");
+        return -1;
+    }
+    if(readConfig(EXEC_CONFIG_FILE, g_exec_config)) {
+        LOG_STDOUT( EXEC_CONFIG_FILE " Error!");
+        return -1;
     }
 
     auto a_loop = new IOLoop();
@@ -160,7 +194,7 @@ int main(int argc, const char * argv[]) {
     a_svr->start(svr.c_str(), config["port"], config["thread"]);
 
 #if defined(SIGPIPE) && !defined(_WIN32)
-    (void)signal(SIGPIPE, SIG_IGN);
+    signal(SIGPIPE, SIG_IGN);
 #endif
 
     a_loop->start_loop();
